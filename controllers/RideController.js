4274353ecc3bc46a17027b9475a77165fb40ac4b
mad6717 <mad6717@gmail.com>
2025-08-05 10:56:09 +0300

    edits in the frontEnd

--- controllers/RideController.js
+++ controllers/RideController.js
@@ -3,12 +3,22 @@ const { Ride } = require('../models')
 const GetRide = async (req, res) => {
   try {
     const Rides = await Ride.find({})
-    res.send(Rides)
+    res.status(200).send(Rides)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetOneRide = async (req, res) => {
+  try {
+    const oneRide = await Ride.findById(req.params.id)
+    res.status(200).send(oneRide)
   } catch (error) {
     throw error
   }
 }
 
 module.exports = {
-  GetRide
+  GetRide,
+  GetOneRide
 }
